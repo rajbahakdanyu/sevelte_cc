e51cc59cc0d90e5bce86a43a4c1f125200e53c38
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-01-11 20:44:20 +0545

    Created 2 components

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,25 @@
 <script>
+    import { FeedbackList } from "./components/FeedbackList.svelte"
+
+    let feedback = [
+        {
+            id: 1,
+            rating: 10,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+        },
+        {
+            id: 2,
+            rating: 9,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+        },
+        {
+            id: 3,
+            rating: 8,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+        },
+    ]
 </script>
 
 <main>
-    <h1>Hello</h1>
+    <FeedbackList />
 </main>
-
-<style>
-</style>
